62d88fdd83483dfec9718665df4efab657be865c
Thien Hoang <hoangkimthien@gmail.com>
2019-08-15 11:00:27 +0700

    up1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+export default class App extends Component {
+
+  
+
+constructor(props) {
+  super(props);
+  const existingToken = sessionStorage.getItem('token');
+  const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+
+  if (!accessToken && !existingToken) {
+    window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+  }
+
+  if (accessToken) {
+    console.log(`New accessToken: ${accessToken}`);
+
+    sessionStorage.setItem("token", accessToken);
+    this.state = {
+      token: accessToken
+    }
+  }
+
+  if (existingToken) {
+    this.state = {
+      token: existingToken
+    };
+  }
 }
 
-export default App;
+componentDidMount = () => {
+  this.fetchUser()
+}
+
+fetchUser = async() => {
+  const response = await fetch('https://api.github.com/users/andyhoang7')
+  const data = await response.json()
+
+  console.log('data', data)
+}
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>GitHub Issues Page</h1>
+      </div>
+    )
+  }
+}
+
